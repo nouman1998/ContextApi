e6ffe06d8f135aa851ff4b5af1c6766ba5516631
nouman1998 <nouman.ejaz198@gmail.com>
2020-08-30 17:04:44 +0500

    Nothing New Commented

--- src/components/nothingNew.jsx
+++ src/components/nothingNew.jsx
@@ -1,13 +1,13 @@
-import {React, Component} from 'react';
+// import {React, Component} from 'react';
 
-function NothingNew() {
-return `<div>
-<h1>Hello</h1>
-<h2>Hello2</h2>
-<h3> Hello 3</h3>
-<h4> Hello4 </h4>
-<h5> Hello5 </h5>
-<h5> Light ni hai </h5>
-</div>`
-}
+// function NothingNew() {
+// return `<div>
+// <h1>Hello</h1>
+// <h2>Hello2</h2>
+// <h3> Hello 3</h3>
+// <h4> Hello4 </h4>
+// <h5> Hello5 </h5>
+// <h5> Light ni hai </h5>
+// </div>`
+// }
  
