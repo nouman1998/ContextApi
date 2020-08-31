8e472e90db2ad8e9577f807cc158e33a1b864ead
nouman1998 <nouman.ejaz198@gmail.com>
2020-08-31 22:10:32 +0500

    App.js updated

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
 import ThemeContextProvider from './context/ThemeContext'
+import AuthContextProvider from './context/AuthContext'
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
       
-    <ThemeContextProvider>  
+    <ThemeContextProvider>
+      <AuthContextProvider>
         <Navbar />
         <BookList />
+        <Button/>
+      </AuthContextProvider>
     </ThemeContextProvider>
      
     </div>
